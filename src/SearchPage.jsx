1f74d0311a8dc7a529bb481f278059329e205ee6
ryan.veitch <ryan.r.veitch@gmail.com>
2016-11-13 19:59:10 -0600

    updated item links and fav icon

--- src/SearchPage.jsx
+++ src/SearchPage.jsx
@@ -7,7 +7,7 @@ import {
 	Hits, HitsStats, NoHits, Pagination, SortingSelector,
 	SelectedFilters, ResetFilters, ItemHistogramList,
 	Layout, LayoutBody, LayoutResults, TopBar,
-	SideBar, ActionBar, ActionBarRow, RangeFilter, SearchkitComponent, TermQuery, FilteredQuery, BoolShould
+	SideBar, ActionBar, ActionBarRow, RangeFilter, CheckboxFilter, SearchkitComponent, TermQuery, FilteredQuery, BoolShould, BoolMust
 } from "searchkit";
 
 require("./index.scss");
@@ -31,7 +31,7 @@ const searchkit = new SearchkitManager(host, {
 
 const MovieHitsGridItem = (props)=> {
   const {bemBlocks, result} = props
-	let url = "http://" + result._source.icon
+	let url = "https://www.bungie.net/en/Armory/Detail?type=item&item=" + result._source.itemHash
   let thumb = "http://" + result._source.icon
 	let img = (thumb == "http://null") ? "http://www.inforum.com/sites/all/themes/inforum_theme/images/touch-icon.png":"https://www.bungie.net" + result._source.icon
   const source:any = _.extend({}, result._source, result.highlight)
@@ -52,7 +52,7 @@ export class SearchPage extends React.Component {
 			<SearchkitProvider searchkit={searchkit}>
 		    <Layout>
 		      <TopBar>
-						<div className="fcc-logo"><img data-qa="fcc-ico" className="fcc-menu-logo" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/446514/destiny_vendor_icon_sm.png" height="40"/></div>
+						<div className="cryptarch-logo"><img data-qa="cryptarch-ico" className="top-bar-menu-logo" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/446514/destiny_vendor_icon_sm.png" height="40"/></div>
 		        <SearchBox
 		          autofocus={true}
 		          searchOnChange={true}
@@ -66,9 +66,23 @@ export class SearchPage extends React.Component {
 									title="Tier"
 									field="tierTypeName.raw"
 									listComponent={ItemHistogramList}/>
+								<CheckboxFilter id="itemtype-weapon" title="Category" label="Weapon" filter={TermQuery("itemType", 3)} />
+								<CheckboxFilter id="itemtype-armor" label="Armor" filter={TermQuery("itemType", 2)} />
+								<CheckboxFilter id="itemtype-item" label="Item" filter={TermQuery("itemType", 0)} />
+								<CheckboxFilter id="itemtype-consumable" label="Consumable" filter={TermQuery("itemType", 9)} />
+								<CheckboxFilter id="itemtype-emblem" label="Emblem" filter={TermQuery("itemType", 14)} />
+								<CheckboxFilter id="itemtype-engram" label="Engram" filter={TermQuery("itemType", 8)} />
+								<CheckboxFilter id="itemtype-currency" label="Currency" filter={TermQuery("itemType", 1)} />
+								<CheckboxFilter id="itemtype-material-exchange" label="Material Exchange" filter={TermQuery("itemType", 10)} />
+								<CheckboxFilter id="itemtype-bounty" label="Bounty" filter={TermQuery("itemType", 4)} />
+								<CheckboxFilter id="itemtype-completed-bounty" label="Completed  Bounty" filter={TermQuery("itemType", 5)} />
+								<CheckboxFilter id="itemtype-bounty-reward" label="Bounty Reward" filter={TermQuery("itemType", 6)} />
+								<CheckboxFilter id="itemtype-quest-step" label="Quest Step" filter={TermQuery("itemType", 12)} />
+								<CheckboxFilter id="itemtype-mission-reward" label="Mission Reward" filter={TermQuery("itemType", 11)} />
+								<CheckboxFilter id="itemtype-message" label="Message" filter={TermQuery("itemType", 7)} />
 								<RefinementListFilter
-									id="item-type"
-									title="Item Type"
+									id="item-type-name"
+									title="Type"
 									field="itemTypeName.raw"
 									size={20}/>
 							</SideBar>
@@ -77,9 +91,7 @@ export class SearchPage extends React.Component {
 		            <ActionBarRow>
 		              <HitsStats/>
 									<SortingSelector options={[
-										{label:"Relevance", field:"_score", order:"desc", defaultOption:true},
-										{label:"Newest", field:"created", order:"desc"},
-										{label:"Oldest", field:"created", order:"asc"}
+										{label:"Relevance:", field:"_score", order:"desc", defaultOption:true}
 									]}/>
 		            </ActionBarRow>
 		            <ActionBarRow>
